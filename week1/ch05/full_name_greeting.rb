a01348b56ff2ea9d18b69f36c91f156c9e3ed731
kristied1224 <kristied1224@berkeley.edu>
2018-06-14 22:32:32 -0700

    Complete full_name_greeting exercise

---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello there friend, what\'s your first name?'
+name = gets.chomp
+puts 'So nice to meet you, ' + name + '. Do you have a middle name? May I ask what it is?'
+middle_name = gets.chomp
+puts 'That\'s such a unique middle name! Also, if you don\'t mind me asking, what\'s your last name?'
+last_name = gets.chomp
+puts 'It\'s an honor to meet you, ' + name + ' ' + middle_name + ' ' + last_name + '.'
