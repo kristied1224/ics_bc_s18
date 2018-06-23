3b0ae323540501be84b3dfe0638de81efa0830d1
kristied1224 <kristied1224@berkeley.edu>
2018-06-23 14:39:03 -0700

    Complete array_sort exercise

---
+++ week2/ch08/array_sort.rb
@@ -0,0 +1,11 @@
+puts 'Type out your favorite words (one word per line): '
+favorite_words = []
+while true
+input = gets.chomp
+  favorite_words.push input
+  if input == ''
+    puts
+    puts favorite_words.sort
+    break
+  end
+end
