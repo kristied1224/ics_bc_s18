bd361c0ef09178e3eccc048dd473c0bb8bec7bd4
kristied1224 <kristied1224@berkeley.edu>
2018-06-23 14:24:14 -0700

    Complete table_of_contents exercise

---
+++ week2/ch06/table_of_contents.rb
@@ -0,0 +1,11 @@
+line_width = 50
+puts ('Table of Contents'.rjust(line_width / 1.1))
+chapter1 = 'Part 1: The Gravedigger\'s Handbook'
+pg1 = 'page  17'
+puts (chapter1.ljust(line_width / 2) + pg1.rjust(line_width / 2.7))
+chapter2 = 'Part 2: The Shoulder Shrug'
+pg9 = 'page  81'
+puts (chapter2.ljust(line_width / 2) + pg9.rjust(line_width / 1.9))
+chapter3 = 'Part 3: Mein Kampf'
+pg13 = 'page 123'
+puts (chapter3.ljust(line_width / 2) + pg13.rjust(line_width / 1.8))
