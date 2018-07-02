fdc58d527072517775407030c749540938167616
kristied1224 <kristied1224@berkeley.edu>
2018-07-01 17:37:09 -0700

    Revision of program with \n

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -1,5 +1,4 @@
-puts 'Lyrics of the song 99 Bottles of Beer'
-puts
+puts "Lyrics of the song 99 Bottles of Beer \n"
 bottle_number = 99
 lyric_line_1_pt1 = ' bottles of beer on the wall, '
 lyric_line_1_pt2 = ' bottles of beer.'
@@ -12,11 +11,9 @@ while true
     bottle_number = (bottle_number.to_i - 1)
   if bottle_number.to_s == '2'.to_s
     puts bottle_number.to_s + lyric_line_1_pt1 + bottle_number.to_s + lyric_line_1_pt2
-    puts lyric_line_2_pt1 + (bottle_number.to_i - 1).to_s + ' bottle of beer on the wall.'
-    puts
+    puts lyric_line_2_pt1 + (bottle_number.to_i - 1).to_s + " bottle of beer on the wall. \n"
     puts (bottle_number.to_i - 1).to_s + ' bottle of beer on the wall, ' + (bottle_number.to_i - 1).to_s + ' bottle of beer.'
-    puts lyric_line_2_pt1 + 'no more bottles of beer on the wall.'
-    puts
+    puts lyric_line_2_pt1 + "no more bottles of beer on the wall. \n"
   puts 'No more bottles of beer on the wall, no more bottles of beer.'
   puts 'Go to the store and buy some more, ' + (bottle_number.to_i + 97).to_s + ' bottles of beer on the wall.'
     break
