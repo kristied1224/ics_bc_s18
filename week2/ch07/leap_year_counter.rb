e88dcee183b509f3c93be161510dfb1cc1583509
kristied1224 <kristied1224@berkeley.edu>
2018-07-01 17:40:24 -0700

    Revision: || instead of 'or'

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -4,7 +4,7 @@ puts 'Ending year:'
 ending = gets.chomp
 puts 'Leap years between designated starting and ending years: '
 while starting.to_i <= ending.to_i
-  if (starting % 4 == 0 and starting % 100 != 0) or (starting % 400 == 0 and starting % 100 == 0)
+  if (starting % 4 == 0 and starting % 100 != 0) || (starting % 400 == 0 and starting % 100 == 0)
     puts starting
   end
   starting = starting.to_i + 1
