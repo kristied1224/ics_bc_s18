69693a77a15ba5ee40f43bcda86df93847d41f6c
kristied1224 <kristied1224@berkeley.edu>
2018-07-06 19:19:49 -0700

    Complete array_min practice

--- week4/recursion_practice/array_min.rb
+++ week4/recursion_practice/array_min.rb
@@ -1,9 +1,15 @@
 def array_min(array, len)
-  if ______________________
-    array[0]
+  if array[0] <= len || array.length == 1
+    puts array[0]
   else
-    ______________________
+    array.push len
+    if array[0] <= array.last
+      array.pop
+    elsif array[0] > array.last
+      array.shift
+      array_min(array, len)
+    end
   end
 end
 
-puts array_min __, __ 
+puts array_min([4, 3, 9], 2)
