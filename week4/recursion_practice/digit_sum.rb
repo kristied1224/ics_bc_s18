b076a0ce9478c91cea2f5fe0537b769e679e86ea
kristied1224 <kristied1224@berkeley.edu>
2018-07-06 19:24:29 -0700

    Complete digit_sum exercise

--- week4/recursion_practice/digit_sum.rb
+++ week4/recursion_practice/digit_sum.rb
@@ -1,9 +1,10 @@
-def digit_sum(num, ___)
-  if ______________________
-    ______________________
+def digit_sum(num)
+  if num.to_i >= 0
+    num.chars.to_a
+    num[0].to_i + num[1].to_i
   else
-    ______________________
+    puts "Please input a number > 0."
   end
 end
 
-puts digit_sum __, __ 
+puts digit_sum ("23")
